cd7faf43913144ee2b1fd8da55449f3feffc2d55
GahSkullCrusher <luis.daniel3003@gmail.com>
2024-09-23 20:31:38 -0600

    Update Prueba.java

--- Estructuras/src/fes/aragon/prueba/Prueba.java
+++ Estructuras/src/fes/aragon/prueba/Prueba.java
@@ -3,6 +3,7 @@ package fes.aragon.prueba;
 public class Prueba {
     public static void main(String[] args) {
         String firts = "77";
+        System.out.println("Hola, creo que ya puedo modificar el código"):
 
         int num = Integer.parseInt(firts);
         System.out.println(num);
