51a7ea57098b4c1b03c6d848885748beba770296
ArianyEstrada <arianyestradaz@gmail.com>
2024-09-26 17:54:19 -0600

    Update ListaSimple.java

--- Estructuras/src/fes/aragon/dinamico/ListaSimple.java
+++ Estructuras/src/fes/aragon/dinamico/ListaSimple.java
@@ -30,13 +30,14 @@ public class ListaSimple <E> {
     }
 
     //Metodo que devuelve true si la lista es vacia, si no un falso
+    //@return true si la lista está vacía (cabeza es null), false en caso contrario.
 
     public boolean esVacia(){
         return cabeza==null;
     }
 
     //Metodo para eliminar nodo de la cabeza
-
+    //Nota: Este método no reduce la longitud de la lista. Debe controlarse fuera de este método.
     public void eliminarCabeza(){
         if(longitud==1){
             cabeza=cola=null;
@@ -46,6 +47,9 @@ public class ListaSimple <E> {
     }
 
     //Metodo para eliminar el nodo de la cola
+    //@param i El índice del nodo que se desea obtener (comienza en 0).
+    //@return El dato almacenado en el nodo en la posición 'i'.
+    // @throws IndiceFueraDeRango Si el índice está fuera de los límites de la lista.
 
     public void EliminarEnCola(){
         if(longitud==1){
@@ -59,6 +63,10 @@ public class ListaSimple <E> {
     }
 
     //Metodo que devuelve el valor de indice i
+    /*@param i El índice del nodo que se desea obtener (comienza en 0).
+    @return El dato almacenado en el nodo en la posición 'i'.
+    @throws IndiceFueraDeRango Si el índice está fuera de los límites de la lista.
+    */
     public E obtenerNodo(int i) throws IndiceFueraDeRango {
         Nodo<E> actual = cabeza;
         int contador = 0;
@@ -79,6 +87,10 @@ public class ListaSimple <E> {
 
     //Metodo que devuelve el indice i si esta el elemento en la lista en dato x,
     // en caso contrario un -1
+    /*
+    @param dato El elemento que se desea buscar en la lista.
+    @return El índice del elemento si se encuentra en la lista; -1 si no está presente.
+    */
     public int estaEnLista (E dato) {
         Nodo<E> actual = cabeza;
         int indice = 0;
@@ -99,6 +111,10 @@ public class ListaSimple <E> {
 
     //Metodo que Elimina el nodo de acuerdo al índice,
     // devuelve un true si lo elimino en caso contrario devuelve un false
+    /*
+    @param i El índice del nodo que se desea eliminar (comienza en 0).
+    @return true si se elimina el nodo correctamente; false si el índice es inválido o no se encuentra el nodo.
+    */
     public boolean eliminarEnIndice (int i){
         if (i < 0 || cabeza == null) {
             return false; // Índice inválido o lista vacía
@@ -131,6 +147,11 @@ public class ListaSimple <E> {
     }
 
     //Metodo que inserta el dato en el indice indicado
+    /*
+    @param i El índice donde se desea insertar el nuevo nodo (comienza en 0).
+    @param dato El dato que se desea almacenar en el nuevo nodo.
+    @return true si se inserta el nodo correctamente; false si el índice es inválido.
+    */
     public boolean insertarEnIndice (int i, E dato){
         if (i < 0) {
             return false; // Índice inválido
@@ -168,6 +189,11 @@ public class ListaSimple <E> {
     }
 
     //Metodo que cambia el dato en el Indice indicado
+    /*
+    @param i El índice del nodo cuyo dato se desea modificar (comienza en 0).
+    @param nuevoDato El nuevo dato que se desea asignar al nodo.
+    @return true si se asigna el nuevo dato correctamente; false si el índice es inválido.
+    */
     public boolean asignar(int i, E nuevoDato){
         if (i < 0 || cabeza == null) {
             return false; // Índice inválido o lista vacía
@@ -197,6 +223,12 @@ public class ListaSimple <E> {
     // si el boolean es true,
     // cambiara todos los datos de la lista simple,
     // si es falso solo cambia el primero que se encuentre
+    /*@param datoActual El dato que se desea modificar en los nodos.
+    @param nuevoDato El nuevo dato que se asignará en lugar del datoActual.
+    @param cambiarTodos Indica si se deben cambiar todas las ocurrencias (true) o solo la primera (false).
+    @return true si al menos una ocurrencia fue modificada; false si la lista está vacía.
+    
+    */
     public boolean asignarR(E datoActual, E nuevoDato, boolean cambiarTodos){
         if (cabeza == null) {
             return false; // Lista vacía
